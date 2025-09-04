ac50716686f1ddd93e6bf1f182f5a50adf95ca10
Ehsan <ehsan.fathi2195@gmail.com>
2025-09-04 22:16:21 +0330

    Add navbar and  components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Contact } from './components/Contact'
+import { Hero } from './components/Hero'
+
+import { Projects } from './components/Projects'
+import { Navbar } from './components/Navbar'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() =>{
+    setIsLoading(true)
+  }, [])
   
 
   return (
-    <>
+    <div className={`app ${isLoading ? "loaded" : ""}`}>
+    <Navbar />
     
+    <Hero />
+    <Projects />
+    <Contact />
+
+    <>
+    <p>&copy; 2025 EhsanFathi. All rights reserved.</p>
     </>
+    </div>
   )
 }
 
