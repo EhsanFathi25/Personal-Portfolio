a5fe3601cd2c4213490b0449f91bdba48514f97a
Ehsan <ehsan.fathi2195@gmail.com>
2025-09-07 23:44:07 +0330

    Add projects component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,12 +36,12 @@ export const Navbar = () => {
             <motion.li
                 variants={fadeInUp} whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.95 }}>
-                <a>Projects</a>
+                <a href="#projects">Projects</a>
             </motion.li>
             <motion.li
                 variants={fadeInUp} whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.95 }}>
-                <a>Contact</a>
+                <a href="#contct">Contact</a>
             </motion.li>
         </motion.ul>
     </motion.nav>)
